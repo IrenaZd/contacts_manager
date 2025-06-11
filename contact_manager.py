5fe81660747fbf318a65aaee4ad136b5e23d713a
Irena <irzd@seznam.cz>
2025-06-11 22:36:41 +0200

    added main function

---
+++ contact_manager.py
@@ -0,0 +1,31 @@
+contacts = [
+   {
+        "name": "Anicka",
+        "email": "anicka@email.com",
+        "phone": "777 777 777"
+   },
+   {
+        "name": "Nikol",
+        "email": "nikol@email.com", 
+        "phone": "777 777 778"
+   }
+]
+
+def hlavni_menu():
+    # vypsala:
+    # 1. zobrazit kontakty
+    # 2. pridat kontakt
+    # 3. ukoncit
+    print("1. zobrazit kontakty")
+    print("2. pridat kontakt")
+    print("3. ukoncit")
+
+    # nacte vstup od uzivatele (input)
+    user_entry = input()
+    # vytiskne
+    print(user_entry)
+
+    # celou implementace commitnout
+
+hlavni_menu()
+
